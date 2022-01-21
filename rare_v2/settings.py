14686c11bf5847eb2274e9bc5ae6cda0381a856c
EugeneTerry <eugene.terry73@gmail.com>
2022-01-20 19:53:02 -0600

    added more startup files

--- rare_v2/settings.py
+++ rare_v2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=a3%m7bs9^!w4i4y(2fv2x^@3u6%g5n&7nb-96a(%qqfrh1a1'
+SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -142,3 +142,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

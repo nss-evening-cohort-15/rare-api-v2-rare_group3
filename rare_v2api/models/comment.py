159aa71eacf0998a0c780780fbaf0c295cecd25a
EugeneTerry <eugene.terry73@gmail.com>
2022-01-24 22:22:35 -0600

    fixed loading errors

--- rare_v2api/models/comment.py
+++ rare_v2api/models/comment.py
@@ -3,7 +3,6 @@ from .post import Post
 from .rareuser import RareUser
 
 class Comment(models.Model):
-
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(RareUser, on_delete=models.CASCADE, related_name="comments")
     content = models.CharField(max_length=250)

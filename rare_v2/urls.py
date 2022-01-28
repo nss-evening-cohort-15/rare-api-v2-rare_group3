6068b0c2641abe19d871e64b43baa7f4229cddcd
EugeneTerry <eugene.terry73@gmail.com>
2022-01-27 21:43:58 -0600

    fixed loading errors

--- rare_v2/urls.py
+++ rare_v2/urls.py
@@ -13,23 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
 from rest_framework import routers
-
 from rare_v2api.views import (
+    PostReactionView,
+    ReactionView,
+    TagView,
+    RareUserView,
     register_user,
     login_user,
-    RareUserView,
 )
-
-from django.conf.urls import include
-from django.urls import path
-from rest_framework import routers
-from django.conf.urls import include 
-from rare_v2api.views import ReactionView
-from rare_v2api.views import PostReactionView
-from rare_v2api.views import TagView
-
-
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'reactions', ReactionView, 'reaction')
@@ -38,10 +33,9 @@ router.register(r'rareusers', RareUserView, 'rareusers')
 router.register(r'tags', TagView, 'tags')
 
 
-router = routers.DefaultRouter(trailing_slash=False)
-
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),

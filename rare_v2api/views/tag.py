b7756c17b6aa858f25658294c2d0b29695ef989b
EugeneTerry <eugene.terry73@gmail.com>
2022-01-29 11:08:00 -0600

    create and delete tags are done

--- rare_v2api/views/tag.py
+++ rare_v2api/views/tag.py
@@ -1,29 +1,55 @@
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rare_v2api.models import Tag
+from rest_framework import status
 
 class TagView(ViewSet):
 
-    def retrieve(self, request, pk=None):
-        try:
-            tag = Tag.objects.get(pk=pk)
-            serializer = TagSerializer(tag, context={'request': request})
-            return Response(serializer.data)
-        except Exception as ex:
-            return HttpResponseServerError(ex)
+  def create(self, request):
+    tag = Tag()
+    tag.label = request.data["label"]
+    
+    try:
+      tag.save()
+      serializer = TagSerializer(tag, context={'request': request})
+      return Response(serializer.data)
+    
+    except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+          
+  def destroy(self, request, pk=None):
+    try:
+      tag = Tag.objects.get(pk=pk)
+      tag.delete()
+      return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    except Tag.DoesNotExist as ex:
+      return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    
+    except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+  
+  def retrieve(self, request, pk=None):
+      try:
+          tag = Tag.objects.get(pk=pk)
+          serializer = TagSerializer(tag, context={'request': request})
+          return Response(serializer.data)
+      except Exception as ex:
+          return HttpResponseServerError(ex)
 
-    def list(self, request):
-      
-        tags = Tag.objects.all()
+  def list(self, request):
+    
+      tags = Tag.objects.all()
 
-        serializer = TagSerializer(
-            tags, many=True, context={'request': request})
-        return Response(serializer.data)
+      serializer = TagSerializer(
+          tags, many=True, context={'request': request})
+      return Response(serializer.data)
 
 class TagSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Tag
-        fields = ('id', 'label')
+  class Meta:
+      model = Tag
+      fields = ('id', 'label')

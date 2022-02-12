9575bb16e83d8e64e1fea66cc88bff5a861389c6
Melody Barker <melodybarker2013@gmail.com>
2022-02-11 19:24:28 -0600

    additional comments and posts

--- rare_v2api/views/rareuser.py
+++ rare_v2api/views/rareuser.py
@@ -10,13 +10,13 @@ class RareUserView(ViewSet):
 
   def retrieve(self, request, pk=None):
     rareuser = request.auth.user.rare_user
-    
+
     serializer = RareUserSerializer(rareuser, context={'request': request})
     return Response(serializer.data)
-  
+
   def list(self, request):
     rearusers = RareUser.objects.all()
-    
+
     serializer = RareUserSerializer(
       rearusers, many=True, context={'request': request})
     return Response(serializer.data)
@@ -25,11 +25,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
       model = User
       fields = ('first_name', 'last_name', 'email','is_superuser', 'username')
-        
+
 class RareUserSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False)
     class Meta:
       model = RareUser
       fields = ('id', 'bio', 'user', 'profile_image_url', 'created_on')
-      
-      
